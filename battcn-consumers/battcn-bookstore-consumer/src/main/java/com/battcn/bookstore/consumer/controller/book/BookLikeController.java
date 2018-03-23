802e1826fc4b0bfe3b661bd8ac40f73cc4603d32
Levin <1837307557@qq.com>
2018-03-23 15:55:35 +0800

    添加点赞接口

--- battcn-consumers/battcn-bookstore-consumer/src/main/java/com/battcn/bookstore/consumer/controller/book/BookLikeController.java
+++ battcn-consumers/battcn-bookstore-consumer/src/main/java/com/battcn/bookstore/consumer/controller/book/BookLikeController.java
@@ -1,5 +1,8 @@
 package com.battcn.bookstore.consumer.controller.book;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.battcn.book.facade.BookLikeService;
+import com.battcn.book.pojo.po.BookLike;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
@@ -13,13 +16,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/app/likes")
 @Api(value = "图书管理", description = "点赞管理", tags = "2.0")
-public class LikeController {
+public class BookLikeController {
+
+    @Reference(version = "1.0.0",
+            application = "${dubbo.application.id}",
+            url = "dubbo://localhost:20880", timeout = 10000)
+    private BookLikeService bookLikeService;
 
     @PutMapping("/{book_no}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @ApiOperation(value = "将喜欢的书籍点赞", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void giveLike(@PathVariable("book_no") String bookNo) {
-
+        BookLike record = new BookLike();
+        record.setBookNo(bookNo);
+        record.setMemberNo("battcn");
+        this.bookLikeService.insertSelective(record);
     }
 
 
---
+++ battcn-facades/battcn-book-facade/src/main/java/com/battcn/book/pojo/po/BookLike.java
@@ -0,0 +1,24 @@
+package com.battcn.book.pojo.po;
+
+import com.battcn.framework.mybatis.pojo.RecordEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Table;
+
+/**
+ * @author Levin
+ * @since 2018/3/23 0023
+ */
+@Table(name = "t_book_like")
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BookLike extends RecordEntity {
+
+    private static final long serialVersionUID = -4334133978678268945L;
+
+    private Long id;
+    private String bookNo;
+    private String memberNo;
+
+}
