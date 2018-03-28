f5dd13924c223419f52191433b5e56520ba5b050
Levin <1837307557@qq.com>
2018-03-28 14:54:10 +0800

    封装 elsasticsearch

---
+++ battcn-framework/battcn-framework-elsasticsearch/src/main/java/com/battcn/framework/elsasticsearch/annotation/EnableElsasticsearch.java
@@ -0,0 +1,18 @@
+package com.battcn.framework.elsasticsearch.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 开启JWT Token
+ *
+ * @author Levin
+ * @since 2018-01-15
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface EnableElsasticsearch {
+
+
+}
