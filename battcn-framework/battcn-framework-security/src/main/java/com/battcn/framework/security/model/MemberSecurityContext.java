39c85fcb401e29a110aecd27453f1f012e1dfac7
1837307557@qq.com <xiongfei>
2018-04-08 18:49:42 +0800

    修改验证逻辑

--- battcn-framework/battcn-framework-security/src/main/java/com/battcn/framework/security/model/MemberSecurityContext.java
+++ battcn-framework/battcn-framework-security/src/main/java/com/battcn/framework/security/model/MemberSecurityContext.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Levin
  * @since 2017-05-25
@@ -12,7 +14,7 @@ import org.hibernate.validator.constraints.NotBlank;
 public class MemberSecurityContext {
 
     @NotBlank(message = "账号不能为空")
-    @Length(min = 6, max = 20, message = "账号长度必须在{min} - {max} 之间")
+    @Pattern(regexp = "^1\\d{10}$", message = "手机号码格式错误")
     private String username;
     @NotBlank(message = "密码不能为空")
     @Length(min = 6, max = 20, message = "密码长度必须在{min} - {max} 之间")
