b299f32c56dc77f96d385eef18e6ec23cf6ac76a
Levin <1837307557@qq.com>
2018-04-09 13:50:45 +0800

    添加修改会员昵称接口

--- battcn-consumers/battcn-bookstore-consumer/src/main/java/com/battcn/bookstore/consumer/controller/member/MemberController.java
+++ battcn-consumers/battcn-bookstore-consumer/src/main/java/com/battcn/bookstore/consumer/controller/member/MemberController.java
@@ -2,15 +2,15 @@ package com.battcn.bookstore.consumer.controller.member;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.battcn.bookstore.consumer.pojo.dto.MemberDetailDto;
+import com.battcn.framework.redis.annotation.CacheLock;
 import com.battcn.framework.security.Environments;
 import com.battcn.member.facade.MemberService;
 import com.battcn.member.pojo.po.Member;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.battcn.bookstore.consumer.pojo.converter.MemberConverts.MEMBER_DETAIL_DTO_CONVERTER;
 
@@ -30,6 +30,16 @@ public class MemberController {
             url = "dubbo://localhost:20880", timeout = 10000)
     private MemberService memberService;
 
+    @CacheLock
+    @ApiOperation(value = "修改会员昵称（DONE）", notes = "修改会员昵称 \n 注：添加了分布式锁,默认5秒")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/nick_name")
+    public void modifyDetail(@RequestParam("nick_name") String nickName) {
+        Member member = new Member();
+        member.setMemberNo(Environments.getAuthId());
+        member.setNickName(nickName);
+        this.memberService.updateSelectiveById(member);
+    }
 
     @ApiOperation(value = "个人资料（DONE）", notes = "获取当前会员的个人资料")
     @GetMapping
