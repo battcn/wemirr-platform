abcb9bf9426a2df921ffccf59999ff8ff5b79c99
Levin <1837307557@qq.com>
2018-04-16 16:52:31 +0800

    优化搜索

--- battcn-consumers/battcn-consumer-bookstore/src/main/java/com/battcn/bookstore/consumer/controller/book/BookController.java
+++ battcn-consumers/battcn-consumer-bookstore/src/main/java/com/battcn/bookstore/consumer/controller/book/BookController.java
@@ -1,8 +1,10 @@
 package com.battcn.bookstore.consumer.controller.book;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSONObject;
 import com.battcn.book.facade.BookChapterService;
 import com.battcn.book.pojo.po.BookChapter;
+import com.battcn.bookstore.consumer.enums.BookRedisEnum;
 import com.battcn.framework.commons.entity.DataGrid;
 import com.battcn.framework.commons.entity.PageResult;
 import com.battcn.framework.security.annotation.IgnoreAuthenticate;
@@ -10,10 +12,15 @@ import com.battcn.search.facade.BookService;
 import com.battcn.search.pojo.po.Book;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 /**
@@ -32,6 +39,13 @@ public class BookController {
             application = "${dubbo.application.id}",
             url = "dubbo://localhost:20882", timeout = 10000)
     private BookChapterService bookChapterService;
+
+    private final RedisTemplate<String, String> redisCacheTemplate;
+
+    @Autowired
+    public BookController(RedisTemplate<String, String> redisCacheTemplate) {
+        this.redisCacheTemplate = redisCacheTemplate;
+    }
 
 
     @IgnoreAuthenticate
@@ -56,6 +70,13 @@ public class BookController {
     }
 
     @IgnoreAuthenticate
+    @PatchMapping("/{book_no}/click")
+    @ApiOperation(value = "图书点击量")
+    public void bookClick(@PathVariable("book_no") String bookNo) {
+        redisCacheTemplate.opsForZSet().incrementScore(BookRedisEnum.BOOKS_CLICK_RANKINGS.getKey(), bookNo, 1);
+    }
+
+    @IgnoreAuthenticate
     @GetMapping(value = "/hots")
     @ApiOperation(value = "热门")
     public List<String> queryHots() {
@@ -65,11 +86,16 @@ public class BookController {
     @IgnoreAuthenticate
     @GetMapping(value = "/recommends")
     @ApiOperation(value = "首页推荐/与总点击榜")
-    public List<Book> queryRecommends() {
+    public JSONObject queryRecommends() {
+        JSONObject result = new JSONObject();
         // TODO 查询 Redis 总点击榜
-        //final List<Serializable> list = cacheService.getList("rankings");
-
-        return this.bookService.selectRecommend();
+        final Set<String> bookClickRankings = redisCacheTemplate.opsForZSet().reverseRange(BookRedisEnum.BOOKS_CLICK_RANKINGS.getKey(), 0, 9);
+        if (!CollectionUtils.isEmpty(bookClickRankings)) {
+            final List<Book> rankings = bookClickRankings.stream().map(bookNo -> this.bookService.selectByBookNo(bookNo)).collect(Collectors.toList());
+            result.put("rankings", rankings);
+        }
+        result.put("recommends", this.bookService.selectRecommend());
+        return result;
     }
 
 }
