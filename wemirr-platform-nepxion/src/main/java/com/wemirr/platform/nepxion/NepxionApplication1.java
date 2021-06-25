c9a4743add454886911afaf6c65af60b05cef65c
memmsc <xiongfei>
2021-06-25 11:09:56 +0800

    feat：优化代码

--- wemirr-platform-nepxion/src/main/java/com/wemirr/platform/nepxion/NepxionApplication1.java
+++ wemirr-platform-nepxion/src/main/java/com/wemirr/platform/nepxion/NepxionApplication1.java
@@ -9,10 +9,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class NepxionApplication {
+public class NepxionApplication1 {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(NepxionApplication.class, args);
+        System.setProperty("spring.profiles.active", "weight1");
+        SpringApplication.run(NepxionApplication1.class, args);
     }
 }
