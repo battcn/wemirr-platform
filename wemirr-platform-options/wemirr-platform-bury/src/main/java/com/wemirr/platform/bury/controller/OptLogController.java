5162c583a5b6f953b998c1432915149ca68d43a5
memmsc <xiongfei>
2021-08-14 14:10:55 +0800

    feat：删除多余包

--- wemirr-platform-options/wemirr-platform-bury/src/main/java/com/wemirr/platform/bury/controller/OptLogController.java
+++ wemirr-platform-options/wemirr-platform-bury/src/main/java/com/wemirr/platform/bury/controller/OptLogController.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Levin
