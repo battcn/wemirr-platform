b32cbc3724c81b65319f1c681fc9236feaee450a
memmsc <xiongfei>
2021-08-25 17:26:02 +0800

    feat：升级 bury 模块

--- wemirr-platform-options/wemirr-platform-bury/src/main/java/com/wemirr/platform/bury/controller/OptLogController.java
+++ wemirr-platform-options/wemirr-platform-bury/src/main/java/com/wemirr/platform/bury/controller/OptLogController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Levin
