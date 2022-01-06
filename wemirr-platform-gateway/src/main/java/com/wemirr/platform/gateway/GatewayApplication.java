5f9f1fae2016a9b669affbeb4f5e9a3ed98db810
唐亚峰 <battcn@cortp.com>
2022-01-06 20:06:25 +0800

    style：添加启动彩蛋

--- wemirr-platform-gateway/src/main/java/com/wemirr/platform/gateway/GatewayApplication.java
+++ wemirr-platform-gateway/src/main/java/com/wemirr/platform/gateway/GatewayApplication.java
@@ -1,9 +1,12 @@
 package com.wemirr.platform.gateway;
 
+import com.wemirr.framework.commons.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * @author Levin
@@ -14,7 +17,15 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(GatewayApplication.class, args);
+        Environment env = applicationContext.getEnvironment();
+        final String appName = env.getProperty("spring.application.name");
+        String port = StringUtils.defaultString(env.getProperty("server.port"), "8080");
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Doc: \thttp://{}:{}/swagger-ui.html\n" +
+                        "----------------------------------------------------------",
+                appName, "localhost", port);
     }
 
 }
