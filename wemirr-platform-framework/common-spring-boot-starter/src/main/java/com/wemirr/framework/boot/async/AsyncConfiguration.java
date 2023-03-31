e3ded5e4d4411d0a16a62e4845f6d12fe1a68505
唐亚峰 <battcn@cortp.com>
2023-03-31 15:25:32 +0800

    feat[I6KX6E]：异步线程支持

---
+++ wemirr-platform-framework/common-spring-boot-starter/src/main/java/com/wemirr/framework/boot/async/AsyncConfiguration.java
@@ -0,0 +1,54 @@
+package com.wemirr.framework.boot.async;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.concurrent.Executor;
+
+/**
+ * 异步线程支持
+ *
+ * @author Levin
+ */
+@Configuration
+@EnableAsync
+public class AsyncConfiguration implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        // 具体可以自己写成 properties 的方式
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("wemirr-async-thread-");
+        executor.setTaskDecorator(new RequestAttributesTaskDecorator());
+        executor.initialize();
+        return executor;
+    }
+
+    private static class RequestAttributesTaskDecorator implements TaskDecorator {
+
+        @NotNull
+        @Override
+        public Runnable decorate(@NotNull Runnable runnable) {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            return () -> {
+                try {
+                    // 在异步方法执行前将 RequestAttributes 绑定到当前线程的 ThreadLocal 中
+                    RequestContextHolder.setRequestAttributes(attributes);
+                    runnable.run();
+                } finally {
+                    // 在异步方法执行后清除当前线程的 ThreadLocal
+                    RequestContextHolder.resetRequestAttributes();
+                }
+            };
+        }
+    }
+}
