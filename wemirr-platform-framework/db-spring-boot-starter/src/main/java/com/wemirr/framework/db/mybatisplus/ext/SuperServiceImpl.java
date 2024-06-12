4e0029a1851f1f556858acd597f8b9cc1747aa97
唐亚峰 | battcn <1837307557@qq.com>
2024-06-12 17:45:02 +0800

    feat：优化 SDK

--- wemirr-platform-framework/db-spring-boot-starter/src/main/java/com/wemirr/framework/db/mybatisplus/ext/SuperServiceImpl.java
+++ wemirr-platform-framework/db-spring-boot-starter/src/main/java/com/wemirr/framework/db/mybatisplus/ext/SuperServiceImpl.java
@@ -19,11 +19,10 @@
 
 package com.wemirr.framework.db.mybatisplus.ext;
 
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -42,69 +41,40 @@ import java.util.List;
  * @since 2020年02月27日18:15:17
  */
 public class SuperServiceImpl<M extends SuperMapper<T>, T> extends ServiceImpl<M, T> implements SuperService<T> {
-    
+
     /**
      * 默认批次提交数量
      */
     private static final int DEFAULT_BATCH_SIZE = 500;
-    
+
     private static final Logger logger = LoggerFactory.getLogger(SuperServiceImpl.class);
-    
+
     public SuperMapper<T> getSuperMapper() {
         if (baseMapper != null) {
             return baseMapper;
         }
         throw new RuntimeException("Mapper类转换异常");
     }
-    
+
     @Override
     public boolean insertBatch(List<T> list) {
-        if (CollectionUtils.isEmpty(list)) {
+        if (CollUtil.isEmpty(list)) {
             logger.warn("请求数据为空....");
             return false;
         }
-        if (list.size() <= DEFAULT_BATCH_SIZE) {
-            return this.baseMapper.insertBatchSomeColumn(list) > 0;
-        }
-        int size = list.size();
-        int i = 1;
-        List<T> recordList = Lists.newArrayList();
-        for (T element : list) {
-            recordList.add(element);
-            if ((i % DEFAULT_BATCH_SIZE == 0) || i == size) {
-                logger.info("数据写入数据库.....");
-                this.baseMapper.insertBatchSomeColumn(recordList);
-                logger.info("清空临时容器内容.....");
-                recordList.clear();
-            }
-            i++;
-        }
+        CollUtil.split(list, DEFAULT_BATCH_SIZE).forEach(baseMapper::insertBatchSomeColumn);
         return true;
     }
-    
+
     @Override
     public boolean updateBatch(List<T> list) {
-        if (CollectionUtils.isEmpty(list)) {
+        if (CollUtil.isEmpty(list)) {
             logger.warn("请求数据为空....");
             return false;
         }
-        if (list.size() <= DEFAULT_BATCH_SIZE) {
-            return this.baseMapper.updateBatchSomeColumnById(list) > 0;
-        }
-        int size = list.size();
-        int i = 1;
-        List<T> recordList = Lists.newArrayList();
-        for (T element : list) {
-            recordList.add(element);
-            if ((i % DEFAULT_BATCH_SIZE == 0) || i == size) {
-                logger.info("数据写入数据库.....");
-                this.baseMapper.updateBatchSomeColumnById(recordList);
-                logger.info("清空临时容器内容.....");
-                recordList.clear();
-            }
-            i++;
-        }
+        CollUtil.split(list, DEFAULT_BATCH_SIZE).forEach(baseMapper::updateBatchSomeColumnById);
         return true;
     }
-    
+
+
 }
