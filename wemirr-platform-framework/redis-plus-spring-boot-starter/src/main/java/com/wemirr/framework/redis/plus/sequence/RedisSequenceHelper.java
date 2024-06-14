d8a57f9d128d411623f862933ff90c1091c4ee35
唐亚峰 | battcn <1837307557@qq.com>
2024-06-14 09:27:31 +0800

    feat：优化SDK

--- wemirr-platform-framework/redis-plus-spring-boot-starter/src/main/java/com/wemirr/framework/redis/plus/sequence/RedisSequenceHelper.java
+++ wemirr-platform-framework/redis-plus-spring-boot-starter/src/main/java/com/wemirr/framework/redis/plus/sequence/RedisSequenceHelper.java
@@ -20,14 +20,11 @@
 package com.wemirr.framework.redis.plus.sequence;
 
 import cn.hutool.core.util.StrUtil;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -36,52 +33,40 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RequiredArgsConstructor
 public class RedisSequenceHelper {
-    
-    private static final DateTimeFormatter YYYYMMDD = DateTimeFormatter.ofPattern("yyyyMMdd");
-    private static final DateTimeFormatter YYYYMM = DateTimeFormatter.ofPattern("yyyyMM");
-    
+
     private final StringRedisTemplate redisTemplate;
-    
-    public String generate(Sequence sequence, KeyRule keyRule, int size) {
-        String localDate;
-        if (keyRule == null) {
-            localDate = null;
-        } else if (keyRule == KeyRule.YYYY_MM) {
-            localDate = LocalDate.now().format(YYYYMM);
-        } else {
-            localDate = LocalDate.now().format(YYYYMMDD);
+
+    public String generate(Sequence sequence) {
+        DateTimeFormatter formatter = sequence.formatter();
+        String localDate = "";
+        if (formatter != null) {
+            localDate = LocalDateTime.now().format(formatter);
         }
         Long increment = redisTemplate.opsForValue().increment(StrUtil.join(sequence.key(), localDate));
-        return StrUtil.join(sequence.prefix(), localDate, StrUtil.padAfter(increment + "", size, '0'));
+        return sequence.prefix()
+                + localDate
+                + StrUtil.blankToDefault(sequence.delimiter(), "")
+                + StrUtil.padPre(increment + "", sequence.size(), '0');
     }
-    
-    public String generate6Number(String key) {
-        String localDate = LocalDate.now().format(YYYYMMDD);
-        Long increment = redisTemplate.opsForValue().increment(key + localDate);
-        return StrUtil.padAfter(increment + "", 6, '0');
-    }
-    
-    public String generateNumber(String key, int size) {
-        Long increment = redisTemplate.opsForValue().increment(key);
-        return StrUtil.padAfter(increment + "", size, '0');
-    }
-    
+
     /**
-     * @author Levin
+     * 按规则生成流水号
+     *
+     * @param sequence     规则
+     * @param isolationKey 隔离（比如 租户ID）
+     * @return 流水号 = 前缀 + 日期时间 + 自增流水号
      */
-    @Getter
-    @NoArgsConstructor
-    @JsonFormat
-    public enum KeyRule {
-        /**
-         * 年月日
-         */
-        YYYY_MM_DD,
-        /**
-         * 年月
-         */
-        YYYY_MM,
-        
+    public String generate(Sequence sequence, Object isolationKey) {
+        DateTimeFormatter formatter = sequence.formatter();
+        String localDate = "";
+        if (formatter != null) {
+            localDate = LocalDateTime.now().format(formatter);
+        }
+        Long increment = redisTemplate.opsForHash().increment(sequence.key() + localDate, isolationKey, 1);
+        return sequence.prefix()
+                + localDate
+                + StrUtil.blankToDefault(sequence.delimiter(), "")
+                + StrUtil.padPre(increment + "", sequence.size(), '0');
     }
-    
+
 }
