d06e418db6a45371d62c7a88eb6f83239838617e
唐亚峰 | battcn <1837307557@qq.com>
2024-06-14 09:22:40 +0800

    feat：优化SDK

--- wemirr-platform-framework/redis-plus-spring-boot-starter/src/main/java/com/wemirr/framework/redis/plus/sequence/Sequence.java
+++ wemirr-platform-framework/redis-plus-spring-boot-starter/src/main/java/com/wemirr/framework/redis/plus/sequence/Sequence.java
@@ -19,23 +19,50 @@
 
 package com.wemirr.framework.redis.plus.sequence;
 
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author Levin
  */
 public interface Sequence {
-    
+
     /**
      * Redis Key
      *
      * @return String
      */
     String key();
-    
+
     /**
      * Redis Prefix
      *
      * @return UN => UN20210909000001
      */
     String prefix();
-    
+
+
+    /**
+     * 流水号的位数，如10 表示 0000000001
+     */
+    default int size() {
+        return 6;
+    }
+
+
+    /**
+     * 分隔符
+     *
+     * @return 分隔符
+     */
+    default String delimiter() {
+        return "";
+    }
+
+    /**
+     * 流水号中日期的格式， null 表示流水号中没有 日期
+     */
+    default DateTimeFormatter formatter() {
+        return DateTimeFormatter.ofPattern("yyyyMMdd");
+    }
+
 }
