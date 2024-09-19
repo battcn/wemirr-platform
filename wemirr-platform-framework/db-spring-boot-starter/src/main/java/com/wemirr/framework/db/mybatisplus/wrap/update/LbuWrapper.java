f33508fb6d303c359237d92270dc58c9615485b2
唐亚峰 | battcn <1837307557@qq.com>
2024-09-19 08:58:11 +0800

    feat：升级 MP 版本

--- wemirr-platform-framework/db-spring-boot-starter/src/main/java/com/wemirr/framework/db/mybatisplus/wrap/update/LbuWrapper.java
+++ wemirr-platform-framework/db-spring-boot-starter/src/main/java/com/wemirr/framework/db/mybatisplus/wrap/update/LbuWrapper.java
@@ -29,6 +29,7 @@ import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -43,13 +44,13 @@ import java.util.function.Consumer;
  */
 public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         implements
-            Update<LbuWrapper<T>, SFunction<T, ?>> {
-    
+        Update<LbuWrapper<T>, SFunction<T, ?>> {
+
     /**
      * SQL 更新字段内容，例如：name='1', age=2
      */
     private final List<String> sqlSet;
-    
+
     /**
      * 不建议直接 new 该实例，使用 Wrappers.lambdaUpdate()
      */
@@ -57,7 +58,7 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         // 如果无参构造函数，请注意实体 NULL 情况 SET 必须有否则 SQL 异常
         this(null);
     }
-    
+
     /**
      * 不建议直接 new 该实例，使用 Wrappers.lambdaUpdate(entity)
      */
@@ -66,7 +67,7 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         super.initNeed();
         this.sqlSet = new ArrayList<>();
     }
-    
+
     /**
      * 不建议直接 new 该实例，使用 Wrappers.lambdaUpdate(...)
      */
@@ -81,7 +82,7 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         this.lastSql = lastSql;
         this.sqlComment = sqlComment;
     }
-    
+
     /**
      * 空值校验
      * 传入空字符串("")时， 视为： 字段名 = ""
@@ -91,7 +92,7 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
     private static boolean checkCondition(Object val) {
         return val != null;
     }
-    
+
     @Override
     public LbuWrapper<T> set(boolean condition, SFunction<T, ?> column, Object val, String mapping) {
         if (condition) {
@@ -100,7 +101,7 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         }
         return this.typedThis;
     }
-    
+
     @Override
     public LbuWrapper<T> setSql(boolean condition, String setSql, Object... params) {
         if (condition && StringUtils.isNotBlank(setSql)) {
@@ -108,7 +109,23 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         }
         return this.typedThis;
     }
-    
+
+    @Override
+    public LbuWrapper<T> setIncrBy(boolean condition, SFunction<T, ?> column, Number val) {
+        return maybeDo(condition, () -> {
+            String realColumn = columnToString(column);
+            sqlSet.add(String.format("%s=%s + %s", realColumn, realColumn, val instanceof BigDecimal ? ((BigDecimal) val).toPlainString() : val));
+        });
+    }
+
+    @Override
+    public LbuWrapper<T> setDecrBy(boolean condition, SFunction<T, ?> column, Number val) {
+        return maybeDo(condition, () -> {
+            String realColumn = columnToString(column);
+            sqlSet.add(String.format("%s=%s - %s", realColumn, realColumn, val instanceof BigDecimal ? ((BigDecimal) val).toPlainString() : val));
+        });
+    }
+
     @Override
     public String getSqlSet() {
         if (CollectionUtils.isEmpty(this.sqlSet)) {
@@ -116,76 +133,76 @@ public class LbuWrapper<T> extends AbstractLambdaWrapper<T, LbuWrapper<T>>
         }
         return String.join(StringPool.COMMA, this.sqlSet);
     }
-    
+
     @Override
     protected LbuWrapper<T> instance() {
         return new LbuWrapper<>(this.getEntity(), this.sqlSet, this.paramNameSeq, this.paramNameValuePairs, new MergeSegments(),
                 SharedString.emptyString(), SharedString.emptyString());
     }
-    
+
     @Override
     public LbuWrapper<T> nested(Consumer<LbuWrapper<T>> consumer) {
         return super.nested(consumer);
     }
-    
+
     @Override
     public LbuWrapper<T> eq(SFunction<T, ?> column, Object val) {
         return super.eq(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> ne(SFunction<T, ?> column, Object val) {
         return super.ne(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> gt(SFunction<T, ?> column, Object val) {
         return super.gt(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> ge(SFunction<T, ?> column, Object val) {
         return super.ge(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> lt(SFunction<T, ?> column, Object val) {
         return super.lt(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> le(SFunction<T, ?> column, Object val) {
         return super.le(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> like(SFunction<T, ?> column, Object val) {
         return super.like(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> notLike(SFunction<T, ?> column, Object val) {
         return super.notLike(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> likeLeft(SFunction<T, ?> column, Object val) {
         return super.likeLeft(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> likeRight(SFunction<T, ?> column, Object val) {
         return super.likeRight(checkCondition(val), column, val);
     }
-    
+
     @Override
     public LbuWrapper<T> in(SFunction<T, ?> column, Collection<?> coll) {
         return super.in(coll != null && !coll.isEmpty(), column, coll);
     }
-    
+
     @Override
     public LbuWrapper<T> in(SFunction<T, ?> column, Object... values) {
         return super.in(values != null && values.length > 0, column, values);
     }
-    
+
 }
