078fc3299ddc98354a319473ea0c6dd10d6fadeb
唐亚峰 | battcn <1837307557@qq.com>
2024-09-26 14:52:00 +0800

    feat：mongo db up

--- wemirr-platform-framework/mongodb-plus-spring-boot-starter/src/main/java/com/wemirr/framework/mongodb/utils/MongoWraps.java
+++ wemirr-platform-framework/mongodb-plus-spring-boot-starter/src/main/java/com/wemirr/framework/mongodb/utils/MongoWraps.java
@@ -2,11 +2,24 @@ package com.wemirr.framework.mongodb.utils;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjUtil;
+import cn.hutool.core.util.ReflectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.google.common.collect.Lists;
+import com.wemirr.framework.commons.exception.CheckedException;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -15,6 +28,7 @@ import java.util.List;
 public final class MongoWraps {
 
     private final List<Criteria> criteriaList;
+    private PageRequest request;
 
     private MongoWraps() {
         this.criteriaList = new ArrayList<>();
@@ -38,17 +52,72 @@ public final class MongoWraps {
         return this;
     }
 
-    public MongoWraps andOr(boolean status, List<String> keys, Object val) {
-        if (!status) {
+    public MongoWraps gt(String key, Object value) {
+        if (ObjUtil.isNotEmpty(value)) {
+            criteriaList.add(Criteria.where(key).gt(value));
+        }
+        return this;
+    }
+
+    public MongoWraps gte(String key, Object value) {
+        if (ObjUtil.isNotEmpty(value)) {
+            criteriaList.add(Criteria.where(key).gte(value));
+        }
+        return this;
+    }
+
+    public MongoWraps lt(String key, Object value) {
+        if (ObjUtil.isNotEmpty(value)) {
+            criteriaList.add(Criteria.where(key).lt(value));
+        }
+        return this;
+    }
+
+    public MongoWraps lte(String key, Object value) {
+        if (ObjUtil.isNotEmpty(value)) {
+            criteriaList.add(Criteria.where(key).lte(value));
+        }
+        return this;
+    }
+
+
+    public MongoWraps in(String key, Collection<?> values) {
+        if (ObjUtil.isNotEmpty(values)) {
+            criteriaList.add(Criteria.where(key).in(values));
+        }
+        return this;
+    }
+
+    public MongoWraps andOr(boolean condition, List<String> keys, Object val) {
+        if (!condition) {
             return this;
         }
         List<Criteria> orList = Lists.newArrayList();
         for (String key : keys) {
+            if (StrUtil.isBlank(key)) {
+                continue;
+            }
             orList.add(Criteria.where(key).is(val));
         }
         Criteria criteria = new Criteria();
         criteria.orOperator(orList);
         criteriaList.add(criteria);
+        return this;
+    }
+
+    public MongoWraps andOperator(boolean condition, Criteria criteria) {
+        if (!condition || criteria == null) {
+            return this;
+        }
+        criteriaList.add(criteria);
+        return this;
+    }
+
+
+    public MongoWraps between(String gteKey, String ltKey, Object getTime, Object ltTime) {
+        if (ObjUtil.isAllNotEmpty(gteKey, ltKey, getTime, ltTime)) {
+            criteriaList.add(Criteria.where(gteKey).gte(getTime).and(ltKey).lt(ltTime));
+        }
         return this;
     }
 
@@ -63,6 +132,61 @@ public final class MongoWraps {
         return this;
     }
 
+    public MongoWraps addCriteria(boolean condition, Criteria criteria) {
+        if (!condition || criteria == null) {
+            return this;
+        }
+        criteriaList.add(criteria);
+        return this;
+    }
+
+
+    public MongoWraps page(boolean condition, PageRequest request) {
+        this.request = request;
+        return this;
+    }
+
+    public MongoWraps page(PageRequest request) {
+        this.request = request;
+        return this;
+    }
+
+    public <T> MongoPageResult<T> execute(MongoTemplate mongoTemplate,
+                                          @NotNull Class<T> entityClass,
+                                          String collectionName) {
+        long count;
+        Query query = this.build();
+        List<T> v2List = Lists.newArrayList();
+        if (request == null) {
+            v2List = mongoTemplate.find(query, entityClass, collectionName);
+            count = v2List.size();
+        } else {
+            count = mongoTemplate.count(query, entityClass, collectionName);
+            if (count > 0) {
+                query.with(request);
+                v2List = mongoTemplate.find(query, entityClass, collectionName);
+            }
+        }
+        return new MongoPageResult<>(v2List, count);
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MongoPageResult<T> {
+
+        /**
+         * 响应结果
+         */
+        private List<T> records;
+
+        /**
+         * 总数
+         */
+        private long total;
+
+    }
+
+
     public Query build() {
         Query query = new Query();
         if (CollUtil.isEmpty(criteriaList)) {
@@ -73,4 +197,38 @@ public final class MongoWraps {
         }
         return query;
     }
+
+    public List<Criteria> toCriteriaList() {
+        if (CollUtil.isEmpty(criteriaList)) {
+            throw CheckedException.notFound("criteria list is empty");
+        }
+        return criteriaList;
+    }
+
+    public Criteria buildCriteria() {
+        if (CollUtil.isEmpty(criteriaList)) {
+            throw CheckedException.notFound("criteria list is empty");
+        }
+        return new Criteria().andOperator(criteriaList);
+    }
+
+
+    public static Update toUpdate(Object object) {
+        final Field[] fields = ReflectUtil.getFields(object.getClass());
+        Update update = new Update();
+        for (Field field : fields) {
+            final String name = field.getName();
+            final MongoId annotation = field.getAnnotation(MongoId.class);
+            if (annotation != null) {
+                continue;
+            }
+            final Object value = ReflectUtil.getFieldValue(object, field);
+            if (ObjectUtils.isEmpty(value)) {
+                continue;
+            }
+            update.set(name, value);
+        }
+        return update;
+    }
 }
+
