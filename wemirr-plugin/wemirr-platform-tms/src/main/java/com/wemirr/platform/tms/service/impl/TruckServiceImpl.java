113e193ef26002b30160db439b0dd9d8502ba1ab
唐亚峰 | battcn <1837307557@qq.com>
2024-12-18 16:11:54 +0800

    feat:优化配置

--- wemirr-plugin/wemirr-platform-tms/src/main/java/com/wemirr/platform/tms/service/impl/TruckServiceImpl.java
+++ wemirr-plugin/wemirr-platform-tms/src/main/java/com/wemirr/platform/tms/service/impl/TruckServiceImpl.java
@@ -54,7 +54,7 @@ public class TruckServiceImpl extends SuperServiceImpl<TruckMapper, Truck> imple
         final Truck driver = Optional.ofNullable(this.baseMapper.selectById(id)).orElseThrow(() -> CheckedException.notFound("司机信息不存在"));
         this.baseMapper.updateById(Truck.builder().id(driver.getId())
                 .approvalStatus(req.getApprovalStatus()).approvalDesc(req.getApprovalDesc())
-                .approvalId(authenticationContext.userId()).approvalName(authenticationContext.realName()).build());
+                .approvalId(authenticationContext.userId()).approvalName(authenticationContext.nickName()).build());
     }
 
     @Override
