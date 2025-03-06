b4712f61915cb6f4f21518da8ea674ec3cde400c
唐亚峰 | battcn <1837307557@qq.com>
2025-03-06 14:26:53 +0800

    feat:演示环境拦截器增强

--- wemirr-platform-gateway/src/main/java/com/wemirr/platform/gateway/filter/DemoGlobalFilter.java
+++ wemirr-platform-gateway/src/main/java/com/wemirr/platform/gateway/filter/DemoGlobalFilter.java
@@ -19,31 +19,47 @@
 
 package com.wemirr.platform.gateway.filter;
 
+import cn.hutool.core.collection.CollUtil;
 import com.wemirr.platform.gateway.utils.MonoHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Levin
  */
 @Slf4j
 @Component
-@Profile("demo")
+//@Profile("demo")
 public class DemoGlobalFilter implements GlobalFilter {
-    
+
+
+    private static final Map<HttpMethod, List<String>> IGNORE_URL = Map.of(HttpMethod.POST, List.of("/resources/create"));
+
+    private boolean isReject(ServerWebExchange exchange) {
+        HttpMethod method = exchange.getRequest().getMethod();
+        List<String> urlList = IGNORE_URL.get(method);
+        if (CollUtil.isEmpty(urlList)) {
+            return false;
+        }
+        String path = exchange.getRequest().getURI().getPath();
+        return urlList.contains(path);
+    }
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpMethod method = exchange.getRequest().getMethod();
-        if (method == HttpMethod.PUT || method == HttpMethod.DELETE) {
+        if (method == HttpMethod.PUT || method == HttpMethod.DELETE || isReject(exchange)) {
             return MonoHelper.wrap(exchange, "演示环境,禁止破坏基础数据,请下载代码自行部署");
         }
         return chain.filter(exchange);
     }
-    
+
 }
