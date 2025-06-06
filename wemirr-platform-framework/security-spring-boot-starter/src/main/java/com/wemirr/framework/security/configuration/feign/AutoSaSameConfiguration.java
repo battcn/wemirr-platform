9b2aa1e65642dcb7fb9a5738c68531da593ef620
YanCh <yanchengheng@medishare.cn>
2025-06-06 17:00:12 +0800

    feat(security): 添加服务间调用自动添加 same-token 功能并增强权限校验
    - 新增 AutoSaSameConfiguration 类实现 Feign 请求拦截器，自动添加 same-token
    - 修改 OAuth2AutoConfiguration 中的权限校验逻辑，支持 same-token 认证
    - 在自动配置文件中添加 AutoSaSameConfiguration
    - 增加 feign-core 依赖以支持 Feign 客户端拦截器

---
+++ wemirr-platform-framework/security-spring-boot-starter/src/main/java/com/wemirr/framework/security/configuration/feign/AutoSaSameConfiguration.java
@@ -0,0 +1,30 @@
+package com.wemirr.framework.security.configuration.feign;
+
+import cn.dev33.satoken.same.SaSameUtil;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * 服务之间调用添加same-token 请求头
+ *
+ * @author YanCh
+ * Created on: 2025-06-06 16:04
+ **/
+@Slf4j
+public class AutoSaSameConfiguration implements RequestInterceptor {
+    @Override
+    public void apply(RequestTemplate template) {
+        final Map<String, Collection<String>> headers = template.headers();
+        if (headers.containsKey(HttpHeaders.AUTHORIZATION)) {
+            log.debug("当前上下文中已包含token,跳过自动获取流程...");
+            return;
+        }
+        // 获取Same Token
+        template.header(SaSameUtil.SAME_TOKEN, SaSameUtil.getToken());
+    }
+}
