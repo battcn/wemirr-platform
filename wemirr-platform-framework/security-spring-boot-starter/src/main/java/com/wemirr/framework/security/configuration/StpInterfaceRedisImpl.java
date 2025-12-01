2d21b653f57c18295e60541a88da6511dd39d3e2
唐亚峰 | battcn <1837307557@qq.com>
2025-12-01 21:14:15 +0800

    fix：移除 StpInterfaceRedisImpl 无效注解

--- wemirr-platform-framework/security-spring-boot-starter/src/main/java/com/wemirr/framework/security/configuration/StpInterfaceRedisImpl.java
+++ wemirr-platform-framework/security-spring-boot-starter/src/main/java/com/wemirr/framework/security/configuration/StpInterfaceRedisImpl.java
@@ -23,7 +23,6 @@ import cn.dev33.satoken.stp.StpInterface;
 import com.wemirr.framework.commons.security.AuthenticationContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -32,13 +31,12 @@ import java.util.List;
  * @author Levin
  */
 @Slf4j
-@Primary
 @Component
 @RequiredArgsConstructor
 public class StpInterfaceRedisImpl implements StpInterface {
-    
+
     private final AuthenticationContext context;
-    
+
     /**
      * 返回一个账号所拥有的权限码集合
      *
@@ -47,11 +45,11 @@ public class StpInterfaceRedisImpl implements StpInterface {
      * @return 返回一个账号所拥有的权限码集合
      */
     @Override
-    
+
     public List<String> getPermissionList(Object loginId, String loginType) {
         return context.funcPermissionList();
     }
-    
+
     /**
      * 返回一个账号所拥有的角色标识集合
      *
