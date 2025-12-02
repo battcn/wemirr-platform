c54d8d7b9350981ec6e5815f7ba7466931af030c
唐亚峰 | battcn <1837307557@qq.com>
2025-12-02 13:57:52 +0800

    feat:优化

--- wemirr-platform-framework/common-framework-core/src/main/java/com/wemirr/framework/commons/security/AuthenticationDetails.java
+++ wemirr-platform-framework/common-framework-core/src/main/java/com/wemirr/framework/commons/security/AuthenticationDetails.java
@@ -4,7 +4,7 @@ package com.wemirr.framework.commons.security;
  * 认证详情 统一返回的数据结构范围
  *
  * @author YanCh
- * Created on: 2025-12-02 10:11
+ * @since 2025-12-02 10:11
  **/
 public interface AuthenticationDetails {
 }
