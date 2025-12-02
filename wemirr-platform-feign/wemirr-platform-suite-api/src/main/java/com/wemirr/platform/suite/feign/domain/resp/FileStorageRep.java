6d4a063c1c77819b812ca443c3316c7c35e1c587
xiao1 <1006963663@qq.com>
2025-12-02 20:40:34 +0800

    feat: 新增文件存储feign

---
+++ wemirr-platform-feign/wemirr-platform-suite-api/src/main/java/com/wemirr/platform/suite/feign/domain/resp/FileStorageRep.java
@@ -0,0 +1,59 @@
+package com.wemirr.platform.suite.feign.domain.resp;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+/**
+ * @author xJh
+ * @date 2025/11/6
+ **/
+@Data
+public class FileStorageRep {
+
+    @Schema(description = "文件访问地址")
+    private String url;
+
+    @Schema(description = "文件大小，单位字节")
+    private Long size;
+
+    @Schema(description = "格式化后的文件大小")
+    private String formatSize;
+
+    @Schema(description = "文件名称")
+    private String filename;
+
+    @Schema(description = "原始文件名")
+    private String originalFilename;
+
+
+
+    /**
+     * 存储路径
+     */
+    private String path;
+
+    /**
+     * 文件扩展名
+     */
+    @TableField(value = "ext")
+    private String ext;
+
+    /**
+     * MIME类型
+     */
+    @TableField(value = "content_type")
+    private String contentType;
+
+    /**
+     * 存储平台
+     */
+    @TableField(value = "platform")
+    private String platform;
+
+
+
+    @Schema(description = "租户ID")
+    private Long tenantId;
+
+}
