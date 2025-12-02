45a506501296865f95b551a1c3f775373532b082
xiao1 <1006963663@qq.com>
2025-12-02 22:17:43 +0800

    feat: 优化启动类

--- wemirr-plugin/wemirr-platform-ai/src/main/java/com/wemirr/platform/ai/AiApplication.java
+++ wemirr-plugin/wemirr-platform-ai/src/main/java/com/wemirr/platform/ai/AiApplication.java
@@ -1,23 +1,47 @@
 package com.wemirr.platform.ai;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.net.InetAddress;
 
 /**
  * @author xJh
  * @since 2025/09/29
  * AI应用启动类
  */
+@Slf4j
+@EnableAsync
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableAsync
 @EnableFeignClients(basePackages = "com.wemirr")
 public class AiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AiApplication.class, args);
+    /**
+     * 启动类.
+     *
+     * @param args 启动参数
+     */
+    @SneakyThrows
+    public static void main(final String[] args) {
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(AiApplication.class, args);
+        Environment env = applicationContext.getEnvironment();
+        final String appName = env.getProperty("spring.application.name");
+        String host = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("""
+                
+                ----------------------------------------------------------
+                \tApplication '{}' is running! Access URLs:
+                \tDoc: \thttp://{}:{}/doc.html
+                ----------------------------------------------------------""",
+                appName, host, port);
     }
 }
